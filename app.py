b60e2a36b30ce39f298ade50dba016cf591044f0
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2020-12-04 14:02:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ import pdfplumber
 def main():
 	docx_file = st.file_uploader("Upload File",type=['txt','docx','pdf'])
 	if docx_file is not None:
-    	try:
-		with pdfplumber.open(docx_file) as pdf:
-			page = pdf.pages[0]
-			st.write(page.extract_text())
-	except:
-		st.write("None")
+    		try:
+			with pdfplumber.open(docx_file) as pdf:
+				page = pdf.pages[0]
+				st.write(page.extract_text())
+		except:
+			st.write("None")
 
 if __name__ == '__main__':
 	main()
