ae9002efefa5ab31ba32a75050098c83d892b7c6
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2020-12-04 13:54:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
 import streamlit as st
-from PyPDF2 import PdfFileReader
+import pdfplumber
 
-def read_pdf(file):
-	pdfReader = PdfFileReader(file)
-	count = pdfReader.numPages
-	all_page_text = ""
-	for i in range(count):
-		page = pdfReader.getPage(i)
-		all_page_text += page.extractText()
-
-	return all_page_text
 
 def main():
-	uploaded_file = st.file_uploader("Choose a file")
-	if uploaded_file is not None:
-		raw_text = read_pdf(uploaded_file)
-		st.write(raw_text)
+	docx_file = st.file_uploader("Upload File",type=['txt','docx','pdf'])
+	if docx_file is not None:
+    	try:
+		with pdfplumber.open(docx_file) as pdf:
+		page = pdf.pages[0]
+		st.write(page.extract_text())
+	except:
+		st.write("None")
 
 if __name__ == '__main__':
 	main()
