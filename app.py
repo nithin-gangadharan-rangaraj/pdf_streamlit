9c6b7e4e3d5b7b1ad6894a7cf2997ce9769afd2d
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2021-01-30 12:23:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,9 +34,10 @@ def scale_fun(data):
 
 def main():
 	st.title("Heart Risk prediction!")
-	image = Image.open('https://wallpapercave.com/wp/wp2314600.jpg')
-	st.image(image, use_column_width=True)
+	#image = Image.open('https://wallpapercave.com/wp/wp2314600.jpg')
+	#st.image(image, use_column_width=True)
 	st.markdown(html, unsafe_allow_html=True)
+	st.markdown("![Alt Text](https://wallpapercave.com/wp/wp2314600.jpg)")
 	age = st.number_input("Enter Age")
 	totChol = st.number_input("Enter Cholesterol")
 	sysBP = st.number_input("Enter Systolic BP")
