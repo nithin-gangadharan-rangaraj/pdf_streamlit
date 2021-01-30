e964c4b304d2c58b7abad9b5eb188c795d6041be
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2021-01-30 14:12:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -33,20 +33,21 @@ def scale_fun(data):
 
 
 def main():
+	st.markdown(html, unsafe_allow_html=True)
 	st.title("Heart Risk Prediction")
 	st.sidebar.title("Sidebar")
+	if(st.sidebar.button("Risk predictor")):
 	#image = Image.open('heart.jpg')
 	#st.image(image, use_column_width=True)
-	st.markdown(html, unsafe_allow_html=True)
-	age = st.number_input("Enter Age")
-	totChol = st.number_input("Enter Cholesterol")
-	sysBP = st.number_input("Enter Systolic BP")
-	diaBP = st.number_input("Enter Diastolic BP")
-	BMI = st.number_input("Enter BMI")
-	heartrate = st.number_input("Enter Heart Rate")
-	glucose = st.number_input("Enter Glucose")
-	x = [age, totChol, sysBP, diaBP, BMI, heartrate, glucose]
-	new = np.array(scale_fun(x))
+		age = st.number_input("Enter Age")
+		totChol = st.number_input("Enter Cholesterol")
+		sysBP = st.number_input("Enter Systolic BP")
+		diaBP = st.number_input("Enter Diastolic BP")
+		BMI = st.number_input("Enter BMI")
+		heartrate = st.number_input("Enter Heart Rate")
+		glucose = st.number_input("Enter Glucose")
+		x = [age, totChol, sysBP, diaBP, BMI, heartrate, glucose]
+		new = np.array(scale_fun(x))
 	
 	#if(st.button("Predict")):
 	#	if (clf_svm.predict(new.reshape(1, -1))==1):
@@ -54,13 +55,13 @@ def main():
 	#	elif (clf_svm.predict(new.reshape(1, -1))==0):
   	#		st.text("Safe")
 	
-	if(st.button("Predict")):
-		probs = clf_svm.predict_proba(new.reshape(1, -1))
-		if (probs[0][1] > probs[0][0]):
-			percentage = probs[0][1] * 100
-			st.write("You are ",round(percentage,2),"% at a risk of developing CVD!")
-		elif (probs[0][0] > probs[0][1]):
-			st.write("No worries, You are safe!")
+		if(st.button("Predict")):
+			probs = clf_svm.predict_proba(new.reshape(1, -1))
+			if (probs[0][1] > probs[0][0]):
+				percentage = probs[0][1] * 100
+				st.write("You are ",round(percentage,2),"% at a risk of developing CVD!")
+			elif (probs[0][0] > probs[0][1]):
+				st.write("No worries, You are safe!")
 
 if __name__ == '__main__':
 	main()
